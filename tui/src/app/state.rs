d72b9bc259e632856bd20cefe838da4a27e21847
erikli <erik@zirkular.io>
2022-06-02 11:57:58 +0200

    rad-tui: Add state for identity

--- tui/src/app/state.rs
+++ tui/src/app/state.rs
@@ -1,5 +1,10 @@
 pub struct Identity {
     pub profile: String,
+    pub name: String,
+    pub urn: String,
+    pub peer_id: String,
+    pub key_hash: String,
+    pub key_full: String,
 }
 
 pub struct Project {
@@ -26,6 +31,11 @@ impl Default for Context {
         Context {
             identity: Some(Identity {
                 profile: "68ddefe3-81dc-431d-b75b-456416c63a4f".to_owned(),
+                name: "test".to_owned(),
+                urn: "rad:git:hnrkqtre3dtutrqkeebcpwwwtxjtonaqp9kuy".to_owned(),
+                peer_id: "hynp6z36munjbjua6ek8kyztr43dotfopo9pwjmnqifcrrpueyihqo".to_owned(),
+                key_hash: "SHA256:ztsGBnbAyhDaoXGAJhw8+EG255MJSNyiWlLCiBfo46o".to_owned(),
+                key_full: "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJvr58uYkhTPHkKOoF4k1kcIlg2H20SsTqlYQjZoBXjo".to_owned(),
             }),
             project: Some(Project {
                 name: "demo".to_owned(),
