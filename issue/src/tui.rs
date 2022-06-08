efc225c8a809caf8726d3b2d00653e33681f1d64
erikli <erik@zirkular.io>
2022-06-08 20:08:53 +0200

    issue: Store application title in tui state

--- issue/src/tui.rs
+++ issue/src/tui.rs
@@ -1,9 +1,16 @@
 use anyhow::Result;
+
 use radicle_terminal as term;
+
 use term::tui::Application;
+use term::tui::store::Value;
+use term::tui::store::{STATE_TITLE};
+
 
 pub fn run() -> Result<()> {
-    let mut application = Application::new("rad issue".to_owned());
+    let mut application = Application::new();
+
+    application.add_state(STATE_TITLE, Value::String("rad-issue".to_owned()));
     application.execute()?;
     
     Ok(())
