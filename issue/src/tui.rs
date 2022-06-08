954698501b2e360eda6b77ae1a6cfca7704a9324
erikli <erik@zirkular.io>
2022-06-08 21:20:23 +0200

    issue: Adjust tui state access

--- issue/src/tui.rs
+++ issue/src/tui.rs
@@ -2,16 +2,13 @@ use anyhow::Result;
 
 use radicle_terminal as term;
 
-use term::tui::Application;
 use term::tui::store::Value;
-use term::tui::store::{STATE_TITLE};
-
+use term::tui::Application;
 
 pub fn run() -> Result<()> {
     let mut application = Application::new();
-
-    application.add_state(STATE_TITLE, Value::String("rad-issue".to_owned()));
+    application.add_state("state.title", Value::String("rad-issue".to_owned()));
     application.execute()?;
-    
+
     Ok(())
 }
