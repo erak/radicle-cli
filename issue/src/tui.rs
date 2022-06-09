9a974c86678c21a5033d52a4895b50f46019c7c6
erikli <erik@zirkular.io>
2022-06-09 11:18:14 +0200

    issue: Update tui title

--- issue/src/tui.rs
+++ issue/src/tui.rs
@@ -9,7 +9,7 @@ use radicle_common::project;
 
 pub fn run(project: &project::Metadata) -> Result<()> {
     let mut application = Application::new();
-    application.add_state("state.title", Value::String("rad".to_owned()));
+    application.add_state("state.title", Value::String("🌱".to_owned()));
     application.add_state("state.project.name", Value::String(project.name.clone()));
     application.execute()?;
 
