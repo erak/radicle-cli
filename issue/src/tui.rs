466dfcb94b2bfb21d99e191f7c14aedac2ab7961
erikli <erik@zirkular.io>
2022-06-09 14:32:38 +0200

    issue: Add empty tui page

--- issue/src/tui.rs
+++ issue/src/tui.rs
@@ -1,17 +1,28 @@
+use std::rc::Rc;
+
 use anyhow::Result;
 
 use radicle_terminal as term;
 
 use term::tui::store::Value;
+use term::tui::window::{EmptyWidget, PageWidget};
 use term::tui::Application;
 
 use radicle_common::project;
 
 pub fn run(project: &project::Metadata) -> Result<()> {
     let mut application = Application::new();
-    application.state().set("state.title", Value::String("🌱".to_owned()));
-    application.state().set("state.project.name", Value::String(project.name.clone()));
-    application.execute()?;
+    application
+        .state()
+        .set("state.title", Value::String("🌱".to_owned()));
+    application
+        .state()
+        .set("state.project.name", Value::String(project.name.clone()));
+
+    let pages = vec![PageWidget {
+        widgets: vec![Rc::new(EmptyWidget)],
+    }];
+    application.execute(pages)?;
 
     Ok(())
 }
